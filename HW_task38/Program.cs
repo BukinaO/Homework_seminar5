be1dbb20039cfcb8d1bc5a72fdaf1a9e170c7f97
Olga Bukina <o.utina2010@gmail.com>
2022-12-06 15:55:32 +0300

    Task 38 added

---
+++ HW_task38/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива. [3 7 22 2 78] -> 76
+int size = ReadInt("Введите размер массива: ");
+double[] numbers = new double[size];
+double Max = 0; 
+double Min = 0;
+double raznost = 0;
+
+FillArrayRandomNumbers(numbers, 0, 100);
+WriteArray(numbers);
+
+FindMinMaxValue();    
+
+raznost = Max - Min;
+
+Console.WriteLine(raznost);
+
+
+void FillArrayRandomNumbers(double[] array, int min, int max)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().NextDouble()*100;
+    }
+}
+
+void WriteArray(double[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void FindMinMaxValue()
+{   
+    Min = numbers[0];
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        if(numbers[i] > Max)
+                Max = numbers[i];
+        if(numbers[i] < Min)
+                Min = numbers[i];
+    }
+}
+
